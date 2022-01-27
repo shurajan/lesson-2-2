d91f0f79300e0020a9aa2078316549522b4c1ece
alexander_bralnin <alexander.bralnin@gmail.com>
2022-01-27 14:29:19 +0300

    minor fix for imports

--- src/main/java/com/geekbrains/Main.java
+++ src/main/java/com/geekbrains/Main.java
@@ -3,9 +3,6 @@ package com.geekbrains;
 import com.geekbrains.exceptions.MyArrayDataException;
 import com.geekbrains.exceptions.MyArraySizeException;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
